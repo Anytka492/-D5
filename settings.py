96dd365e9c6ea02b90bd81cce6f10e823f45fa03
Anytka492 <126511027+Anytka492@users.noreply.github.com>
2023-12-24 11:01:47 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -185,5 +185,6 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
         'LOCATION': os.path.join(BASE_DIR, 'cache_files'),
+	'TIMEOUT': 30,
     }
 }
